4b7605aa132d2d44e1d72ed8beb23e92c2d2f069
refatull <refatull@buffalo.edu>
2021-12-06 06:27:36 -0500

    handles the backend of updating the profile pic

--- myapp/uploader.js
+++ myapp/uploader.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const multer = require('multer');
+var cookieParser = require('cookie-parser');
+const pgp = require("pg-promise")();
+const db = pgp("postgres://postgres:postgres@postgres:5432/postgres");
+const bcrypt = require('bcrypt');
 const myusers = require('./users.js')
 
 const router = express.Router()
@@ -24,9 +28,28 @@ const imageUpload = multer({
     }
 })  
 
+router.use(cookieParser());
+
 router.post('/image-upload', imageUpload.single('image'), (req, res) => {
-    console.log(myusers.addImage(req.body.username,req.body.image))
-    res.send(req.file)
+
+    const token = req.cookies['Authentication']
+    db.any(`SELECT token,username from "user";`)
+        .then(data => {
+            data.map(u => {
+                if (u.token != null && token != null) {
+                    console.log(token, u.token)
+                    bcrypt.compare(token, u.token).then((match) => {
+                        if (match) {
+                            console.log(u.username)
+                            myusers.addImage(u.username, req.file.originalname)
+                        }
+                    })
+                }
+            })
+        })
+
+    res.redirect(301, "/feed")
+
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
